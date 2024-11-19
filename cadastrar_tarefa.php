8e48f10710565563db647177eae6fbf559b827b7
Gabriel Luis de Santana <131803001+gabrieldsantana28@users.noreply.github.com>
2024-11-19 15:04:00 -0300

    Update cadastrar_tarefa.php

--- cadastrar_tarefa.php
+++ cadastrar_tarefa.php
@@ -1,36 +1,36 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "gerenciador_tarefas";
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "gerenciador_tarefas";
 
-$conn = new mysqli($servername, $username, $password, $dbname);
+    $conn = new mysqli($servername, $username, $password, $dbname);
 
-if ($conn->connect_error) {
-    die("Conexão falhou: " . $conn->connect_error);
-}
-
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $motorista_id = $_POST['motorista'];
-    $descricao = $_POST['descricao'];
-    $rota = $_POST['rota'];
-    $prioridade = $_POST['prioridade'];
-    $data_cadastro = date('Y-m-d');
-
-    $sql = "INSERT INTO viagens (id_motorista, descricao, rota, prioridade, data_cadastro) 
-            VALUES ('$motorista_id', '$descricao', '$rota', '$prioridade', '$data_cadastro')";
-
-    if ($conn->query($sql) === TRUE) {
-        echo "<h1><center>Tarefa cadastrada com sucesso.</center></h1>";
-        echo "<br>";
-        echo "<form action='index.php' method='get' style='display:inline;'>";
-        echo "<center><button type='submit' class='btn'>Voltar ao Menu</button></center>";
-        echo "</form>";
-    } else {
-        echo "Erro: " . $sql . "<br>" . $conn->error;
+    if ($conn->connect_error) {
+        die("Conexão falhou: " . $conn->connect_error);
     }
-}
 
-// Fecha a conexão
-$conn->close();
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $usuario_id = $_POST['usuario'];
+        $descricao = $_POST['descricao'];
+        $setor = $_POST['setor'];
+        $prioridade = $_POST['prioridade'];
+        $data_cadastro = date('Y-m-d');
+
+        $sql = "INSERT INTO tarefa (fk_id_usuario, descricao_tarefa, setor_tarefa, prioridade_tarefa, data_cadastro_tarefa) 
+                VALUES ('$usuario_id', '$descricao', '$setor', '$prioridade', '$data_cadastro')";
+
+        if ($conn->query($sql) === TRUE) {
+            echo "<h1><center>Tarefa cadastrada com sucesso.</center></h1>";
+            echo "<br>";
+            echo "<form action='index.php' method='get' style='display:inline;'>";
+            echo "<center><button type='submit' class='btn'>Voltar ao Menu</button></center>";
+            echo "</form>";
+        } else {
+            echo "Erro: " . $sql . "<br>" . $conn->error;
+        }
+    }
+
+    // Fecha a conexão
+    $conn->close();
 ?>
