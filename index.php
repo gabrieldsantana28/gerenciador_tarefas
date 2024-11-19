27aef6daccfc922517f0df611633e2fd0fa1d5df
Thauã Brandão <134437322+ThauaBrandao@users.noreply.github.com>
2024-11-19 15:43:56 -0300

    Menu feito 100%
    Menu (php e css) feitos

--- index.php
+++ index.php
@@ -5,8 +5,19 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Menu</title>
+    <link rel="stylesheet" href="style/index.css">
 </head>
 <body>
-    
+    <header>
+        <h1>Gerenciamento de Tarefas</h1>
+    </header>
+    <div>
+        <h2>MENU</h2>
+    </div>
+    <section>
+        <button class="btn" onclick="window.location.href='Form_CadastroUsuario.php';">Cadastro de Usuários</button>
+        <button class="btn" onclick="window.location.href='Form_CadastroTarefas.php';">Cadastro de Tarefas</button>
+        <button class="btn" onclick="window.location.href='Form_GerenciamentoTarefas.php';">Gerenciar Tarefas</button>
+    </section>
 </body>
 </html>
