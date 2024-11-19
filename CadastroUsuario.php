87dc0a0f36065a9b9bf7b1b1d750cecb1f9b9e52
Thauã Brandão <134437322+ThauaBrandao@users.noreply.github.com>
2024-11-19 14:30:37 -0300

    CadastroUsuario.php
    update

---
+++ CadastroUsuario.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cadastro de Usuários</title>
+    <link rel="stylesheet" href="style/CadastroUsuario.css">
+</head>
+<body>
+    <header >
+        <h1>Gerenciamento de Tarefas</h1>
+    </header>
+    <section>
+    <h1 class="title">Cadastro de Usuários</h1>
+        <form action="">
+            <div>
+                <label for="nome">Nome:</label> <br>
+                <input type="text" name="nome" id="nome">
+            </div>
+            <div>
+                <label for="email">Email:</label> <br>
+                <input type="email" name="email" id="email">
+            </div>
+            <button>Cadastrar</button>
+        </form>
+    </section>
+</body>
+</html>
